9bb7f8709d9f4b7f35f3fedc0be08dc2d8422a09
Elian Braja <elian@motomtech.com>
2023-03-06 11:24:06 +0100

    Get and display profile data

--- src/components/LinkedInOauth/LinkedInOauth.js
+++ src/components/LinkedInOauth/LinkedInOauth.js
@@ -1,34 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-import { useLinkedIn } from 'react-linkedin-login-oauth2';
+import {useLinkedIn} from 'react-linkedin-login-oauth2';
 import linkedin from 'react-linkedin-login-oauth2/assets/linkedin.png';
+import ProfileCard from "./ProfileCard";
 import axios from "axios";
 
 function LinkedInOauth() {
 
-  const createTokens = code => {
-    axios.post('/api/create-tokens-linkedin', {code})
-        .then(response => {
-          console.log(response.data)
-        })
-        .catch(error => console.log(error.message))
-  }
+    const [authorized, setAuthorized] = useState(false)
+    const [firstName, setFirstName] = useState(null)
+    const [lastName, setLastName] = useState(null)
+    const [pictureUrl, setPictureUrl] = useState(null)
+    const createTokens = code => {
+        axios.post('/api/create-tokens-linkedin', {code})
+            .then(response => {
+                setAuthorized(true)
+                setFirstName(response.data.localizedFirstName)
+                setLastName(response.data.localizedLastName)
+                setPictureUrl(response.data.profilePicture["displayImage~"].elements[0].identifiers[0].identifier)
+            })
+            .catch(error => console.log(error.message))
+    }
 
-  const { linkedInLogin } = useLinkedIn({
-    clientId: process.env.REACT_APP_LINKEDIN_CLIENT_ID,
-    redirectUri: process.env.REACT_APP_LINKEDIN_REDIRECT_URI,
-    scope: "profile",
-    onSuccess: code => createTokens(code),
-  });
+    const {linkedInLogin} = useLinkedIn({
+        clientId: process.env.REACT_APP_LINKEDIN_CLIENT_ID,
+        redirectUri: process.env.REACT_APP_LINKEDIN_REDIRECT_URI,
+        scope: "r_liteprofile",
+        onSuccess: code => createTokens(code),
+    });
 
-  return (
-    <img
-      onClick={() => linkedInLogin()}
-      src={linkedin}
-      alt="Sign in with Linked In"
-      style={{ maxWidth: '180px', cursor: 'pointer' }}
-    />
-  );
+    return (
+        <div>
+            {
+                authorized ?
+                    <ProfileCard
+                        firstName={firstName}
+                        lastName={lastName}
+                        pictureURL={pictureUrl}
+                    />
+                    :
+                    <img
+                        onClick={() => linkedInLogin()}
+                        src={linkedin}
+                        alt="Sign in with Linked In"
+                        style={{maxWidth: '180px', cursor: 'pointer'}}
+                    />
+            }
+        </div>
+    );
 }
 
 export default LinkedInOauth;
--- src/components/LinkedInOauth/ProfileCard.js
+++ src/components/LinkedInOauth/ProfileCard.js
@@ -1,18 +1,14 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class ProfileCard extends Component {
-
-  render(){
+function ProfileCard(props) {
     return (
-      <div className="profile">
-        <div className="profile-container">
-          <img src={this.props.pictureURL} alt="" height="200px" width="200px"/>
-          <h1><a href={this.props.profileURL} target="_blank" rel="noopener noreferrer">{this.props.firstName} {this.props.lastName}</a></h1>
-          <h2>{this.props.headline}</h2>
+        <div className="profile">
+            <div className="profile-container">
+                <img src={props.pictureURL} alt="" height="200px" width="200px"/>
+                <h1>{props.firstName} {props.lastName}</h1>
+            </div>
         </div>
-      </div>
     )
-  }
 }
 
 export default ProfileCard;
